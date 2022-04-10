fbe8ec45c56c4109bf675d001f0575e0a17083ee
Prateek Shukla <200108043@hbtu.ac.in>
2022-04-10 23:16:20 +0530

    connecting project to mongodb via mongoose

--- app.js
+++ app.js
@@ -1,22 +1,59 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
-
+const mongoose = require("mongoose")
 const app = express();
 
-const items =["Go to market"];
-const workItems =[];
-
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", function(req,res){
+main().catch(err => console.log(err))
 
+async function main(){
+    await mongoose.connect('mongodb://localhost:27017/todoListDB')
+}
+
+const itemsSchema = new mongoose.Schema({
+    name : String
+})
+
+const item = mongoose.model("Item", itemsSchema);
+
+const item1 = new item({
+    name: "Welcome to my TodoList"
+})
+const item2 = new item({
+    name: "Click + button to add new task"
+})
+const item3 = new item({
+    name: "Click on the checkbox to remove a task"
+})
+
+const defaultItems = [item1,item2,item3];
+
+app.get("/", function(req,res){
     const day = date.getDate();
 
-    res.render("list", {listTitle: day, newListItems: items});
-})
+    item.find({}, function(err,foundItems){
+        
+        if(foundItems.length==0){
+            item.insertMany(defaultItems, function(err){
+                if(err){
+                    console.log(err)
+                }
+                else{
+                     console.log("Successfully added default items!")
+                }
+            });
+            res.redirect("/");
+        }
+        else{
+            res.render("list", {listTitle: day, newListItems: foundItems})
+        }
+    });
+});
 
 app.post("/", function(req,res){
 
